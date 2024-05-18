a6ecb9be9d01bf7360586c2db3030f3df523ab76
BKALDAS <bernardogcaldas@gmail.com>
2024-05-18 01:04:21 +0100

    signUp form

--- View/src/app/component/menubar/menubar.component.ts
+++ View/src/app/component/menubar/menubar.component.ts
@@ -1,29 +1,31 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginPopUpComponent } from 'src/app/login-pop-up/login-pop-up.component';
+import { RegisterPopUpComponent } from 'src/app/register-pop-up/register-pop-up.component'; // Add this import
 
 @Component({
   selector: 'app-menubar',
   templateUrl: './menubar.component.html',
   styleUrls: ['./menubar.component.css']
 })
-
 export class MenubarComponent {
   badgevisible = false;
   
   constructor(private dialog: MatDialog) {}
 
-
   badgevisibility() {
     this.badgevisible = true;
   }
 
-  userLogin(){
-    this.Openpopup(0, 'User LogIn',LoginPopUpComponent);
+  userLogin() {
+    this.Openpopup(0, 'User LogIn', LoginPopUpComponent);
   }
-  
-  
-  Openpopup(code: any, title: any,component:any) {
+
+  openSignupDialog() {
+    this.Openpopup(0, 'User Signup', RegisterPopUpComponent); // Add this method
+  }
+
+  Openpopup(code: any, title: any, component: any) {
     var _popup = this.dialog.open(component, {
       width: '40%',
       enterAnimationDuration: '800ms',
@@ -34,12 +36,8 @@ export class MenubarComponent {
       }
     });
     _popup.afterClosed().subscribe(item => {
-      // console.log(item)
-      //this.loadcustomer();
-    })
+      // Handle any actions after the dialog closes
+      // this.loadcustomer(); // Example: reload customer data
+    });
   }
-
 }
-
-
-
