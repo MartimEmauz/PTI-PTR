cba6a90330cb5f5b9cf8f2ab0fcaaa94463f6164
BKALDAS <bernardogcaldas@gmail.com>
2024-06-13 17:47:33 +0100

    big objects progress

--- View/src/app/component/myleiloes/myleiloes.component.ts
+++ View/src/app/component/myleiloes/myleiloes.component.ts
@@ -17,12 +17,18 @@ export class MyLeiloesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   newLostObject: LostObject = {
+    title:"",
     date: '',
     description: '',
     category: null,
-    address: null
   };
   showAddObjectForm: boolean = false;
+
+  
+  searchText: string = '';
+  lostObjects: any[] = [];
+  filteredObjects: any[] = [];
+
 
   constructor(private service: MasterService) {
     this.dataSource = new MatTableDataSource<any>();
@@ -30,6 +36,11 @@ export class MyLeiloesComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadCustomer();
+    this.service.getLostObjects().subscribe((data: any[]) => {
+      this.lostObjects = data;
+      this.filteredObjects = data;
+    });
+
   }
 
   loadCustomer() {
@@ -53,21 +64,56 @@ export class MyLeiloesComponent implements OnInit {
     // Lógica para detalhar cliente
   }
 
-  addLostObject() {
+  addLostObject() {                                      
+    console.log(this.newLostObject)
     this.service.addLostObject(this.newLostObject).subscribe(() => {
       this.loadCustomer();
       this.cancelAddObject();
+      window.location.reload();
     });
   }
 
   cancelAddObject() {
     this.newLostObject = {
+      title:"",
       date: '',
       description: '',
       category: null,
-      address: null
     };
     this.showAddObjectForm = false;
   }
+
+
+
+  onSearch() {
+    if (this.searchText.trim() === '') {
+      this.filteredObjects = [...this.lostObjects];
+    } else {
+      this.filteredObjects = this.lostObjects.filter(obj =>
+        obj.title.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+      // Adicione os objetos que não correspondem à pesquisa
+      const remainingObjects = this.lostObjects.filter(obj =>
+        !obj.title.toLowerCase().includes(this.searchText.toLowerCase())
+      );
+      this.filteredObjects.push(...remainingObjects);
+    }
+  }
+
+  getCardImagePath(categoryId: number): string {
+    switch (categoryId) {
+      case 1:
+        return "assets/technology.jpg";
+      case 2:
+        return "assets/clothes.jpg";
+      case 3:
+        return "assets/acessories.jpeg";
+      case 4:
+        return "assets/sport.jpg";
+      default:
+        return ""; // ou o caminho para uma imagem padrão caso a categoria não seja reconhecida
+    }
+  }
+
 
 }
