ea90896f01076cee72c2c8fcd96cd77475d00fbe
fc58629 <fc58629@alunos.fc.ul.pt>
2024-06-20 18:55:16 +0100

    a dar

--- View/src/app/component/slider/slider.component.ts
+++ View/src/app/component/slider/slider.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
-  center: google.maps.LatLngLiteral = { lat: -34.397, lng: 150.644 };
-  zoom = 8;
-  options: google.maps.MapOptions = {
-    mapTypeId: 'roadmap',
-    disableDefaultUI: true,
-    maxZoom: 18,
-    minZoom: 8,
-  };
+  
 }
