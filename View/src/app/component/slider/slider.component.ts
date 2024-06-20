05821a7a59e7d41bd422bc058cf69058fd088852
fc58629 <fc58629@alunos.fc.ul.pt>
2024-06-20 18:45:56 +0100

    reset

--- View/src/app/component/slider/slider.component.ts
+++ View/src/app/component/slider/slider.component.ts
@@ -54,6 +54,9 @@ export class SliderComponent {
   startvalue=30;
   endvalue=70;
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 6b1419d (fixing)
+=======
 >>>>>>> parent of 6b1419d (fixing)
 =======
 >>>>>>> parent of 6b1419d (fixing)
