a7a0e975d15d17911032b99c519b2a35066b4e35
fc58629 <fc58629@alunos.fc.ul.pt>
2024-06-20 18:53:32 +0100

    A dar

--- View/src/app/component/slider/slider.component.ts
+++ View/src/app/component/slider/slider.component.ts
@@ -1,50 +1,17 @@
-import { Component, ElementRef, NgZone, AfterViewInit, ViewChild } from '@angular/core';
-
-declare var google: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent implements AfterViewInit {
-  @ViewChild('mapContainer', {static: false}) gmap = ElementRef<any>;
-  map = google.maps.Map;
-  lat = -23.5505; // Exemplo de latitude (por exemplo, São Paulo)
-  lng = -46.6333; // Exemplo de longitude
-  zoom = 12; // Zoom inicial
-
-  constructor(private zone: NgZone) {}
-
-  ngAfterViewInit() {
-    if (this.gmap) {
-      this.loadGoogleMapsScript(() => {
-        this.initMap();
-      });
-    } else {
-      console.error('Elemento mapContainer não encontrado.');
-    }
-  }
-
-  loadGoogleMapsScript(callback: () => void): void {
-    const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY`;
-    script.onload = () => {
-      this.zone.run(callback);
-    };
-    document.body.appendChild(script);
-  }
-
-  initMap(): void {
-    const mapOptions: google.maps.MapOptions = {
-      center: { lat: this.lat, lng: this.lng },
-      zoom: this.zoom
-    };
-    this.map = new google.maps.Map(this.gmap.nativeElement, mapOptions);
-    new google.maps.Marker({
-      position: { lat: this.lat, lng: this.lng },
-      map: this.map,
-      title: 'Marker'
-    });
-  }
+export class SliderComponent {
+  center: google.maps.LatLngLiteral = { lat: -34.397, lng: 150.644 };
+  zoom = 8;
+  options: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    disableDefaultUI: true,
+    maxZoom: 18,
+    minZoom: 8,
+  };
 }
