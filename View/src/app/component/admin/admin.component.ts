2d02c8039b368b17669d3207d23163256efd3c96
André Silva <andre.j.silva.11@mail.com>
2024-06-27 12:48:08 +0100

    Admin done

--- View/src/app/component/admin/admin.component.ts
+++ View/src/app/component/admin/admin.component.ts
@@ -1,65 +1,110 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MasterService } from '../../service/master.service';
-import { PolicePost } from '../../Model/postopolice.model';
 import { Address } from '../../Model/address.model';
+import { PolicePost } from '../../Model/postopolice.model';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit{
-
+export class AdminComponent implements OnInit {
+  policePostForm: FormGroup;
   policePosts: PolicePost[] = [];
-  postForm: FormGroup;
 
-  constructor(private masterService: MasterService, private fb: FormBuilder) {
-    this.postForm = this.fb.group({
-      stationnumber: ['', Validators.required],
+  constructor(
+    private fb: FormBuilder,
+    private masterService: MasterService,
+    private router: Router
+  ) {
+    this.policePostForm = this.fb.group({
+      stationnumber: ['', [Validators.required, Validators.pattern(/^\d+$/)]],
       street: ['', Validators.required],
-      country: ['', Validators.required],
-      city: ['', Validators.required],
-      zip: ['', Validators.required]
+      country: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)]],
+      city: ['', [Validators.required, Validators.pattern(/^[a-zA-Z\s]+$/)]],
+      zip: ['', [Validators.required, Validators.pattern(/^\d{4}-\d{3}$/)]]
     });
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.loadPolicePosts();
   }
 
-  loadPolicePosts(): void {
-    this.masterService.getPolicePosts().subscribe(posts => {
-      this.policePosts = posts;
+  loadPolicePosts() {
+    this.masterService.getPolicePosts().subscribe(
+      (data) => {
+        this.policePosts = data;
+        this.loadAddresses();
+      },
+      (error) => {
+        console.error('Error fetching police posts:', error);
+      }
+    );
+  }
+
+  loadAddresses() {
+    this.policePosts.forEach((post) => {
+      if (post.location && typeof post.location === 'number') {
+        this.masterService.getAddressById(post.location).subscribe(
+          (address) => {
+            post.location = address;
+          },
+          (error) => {
+            console.error('Error fetching address:', error);
+          }
+        );
+      }
     });
   }
 
-  onSubmit(): void {
-    if (this.postForm.valid) {
+  onSubmit() {
+    if (this.policePostForm.valid) {
       const address: Address = {
-        street: this.postForm.value.street,
-        country: this.postForm.value.country,
-        city: this.postForm.value.city,
-        zip: this.postForm.value.zip
+        street: this.policePostForm.value.street,
+        country: this.policePostForm.value.country,
+        city: this.policePostForm.value.city,
+        zip: this.policePostForm.value.zip,
       };
 
-      const newPost: PolicePost = {
-        id: 0,
-        stationnumber: this.postForm.value.stationnumber,
-        location: address
-      };
+      this.masterService.createAddress(address).subscribe(
+        (addressResponse) => {
+          const policePost: PolicePost = {
+            stationnumber: this.policePostForm.value.stationnumber,
+            location: addressResponse.id
+          };
 
-      this.masterService.addPolicePost(newPost).subscribe(post => {
-        this.policePosts.push(post);
-        this.postForm.reset();
-      });
+          this.masterService.addPolicePost(policePost).subscribe(
+            (policePostResponse) => {
+              this.loadPolicePosts();
+            },
+            (error) => {
+              console.error('Error creating police post:', error);
+            }
+          );
+        },
+        (error) => {
+          console.error('Error creating address:', error);
+        }
+      );
+    } else {
+      console.error('Form is invalid');
     }
   }
 
-  deletePost(id: number): void {
-    this.masterService.deletePolicePost(id).subscribe(() => {
-      this.policePosts = this.policePosts.filter(post => post.id !== id);
-    });
+  deletePolicePost(id: number) {
+    this.masterService.deletePolicePost(id).subscribe(
+      () => {
+        this.loadPolicePosts();
+      },
+      (error) => {
+        console.error('Error deleting police post:', error);
+      }
+    );
+  }
+
+  isAddress(location: number | Address | undefined): location is Address {
+      return (location as Address).street !== undefined;
   }
 }
-
